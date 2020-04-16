7e43597fbbf66c2561d6e4a5d2956b071819a27b
vikram123-lab <63367455+vikram123-lab@users.noreply.github.com>
2020-04-16 14:21:45 +0530

    code updated
    Congratulations project done

--- PasswordValidation.py
+++ PasswordValidation.py
@@ -1,5 +1,33 @@
-will update the code very soon
+#code updated very soon
+#password validation project
+#mini project
+#condition
+#length of the password should be in between 6 to 12 both included
+#atleast one [a-z]
+#atleast one [A-Z]
+#atleast one [0-9]
+#atleast one [@#$%&]
 import re
+import sys
+pwd = input("enter the password")
+while True:
+    if len(pwd)<6 or len(pwd)>12:
+        break
+    elif not re.search('[a-z]',pwd):
+        break
+    elif not re.search('[0-9]',pwd):
+        break
+    elif not re.search('[A-Z]',pwd):
+        break
+    elif not re.search('[@#$%&]',pwd):
+        break
+    elif re.search('\s',pwd):
+        break
+    else:
+        print("valid")
+        sys.exit()
+
+print("invalid")
 
 
 
@@ -10,4 +38,4 @@ import re
 
 
 
-thanks
+
